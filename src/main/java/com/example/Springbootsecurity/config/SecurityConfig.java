f39e3460dfc54b865f30f9af1d8524e8701f7821
manoj <petlavenkatamanoj@gmail.com>
2025-07-28 18:22:23 +0530

    added controller  to make  project more readable and scalable

--- src/main/java/com/example/Springbootsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/Springbootsecurity/config/SecurityConfig.java
@@ -2,9 +2,12 @@ package com.example.Springbootsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,5 +21,12 @@ public class SecurityConfig {
         http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider=new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
+        provider.setUserDetailsService(userDetailsService);
     }
 }
---
+++ src/main/java/com/example/Springbootsecurity/userDetailsService.java
@@ -0,0 +1,5 @@
+package com.example.Springbootsecurity;
+
+public class userDetailsService {
+
+}
